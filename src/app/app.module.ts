d865f6a54f5757f3834841557f030e6b7c681c36
larissamiguel <74651820+LarissaMiguelTsatedem@users.noreply.github.com>
2021-03-31 21:12:54 +0200

    Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent,
+    CrisisListComponent,
+    HeroesListComponent,
+    PageNotFoundComponent,
+    HeroListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
